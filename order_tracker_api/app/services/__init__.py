7ed9fd70635fec0eb53f6f98461e518373c20fb0
membranegaming <vardhman8949@gmail.com>
2026-06-27 16:23:03 +0530

    fix: rename refresh_access_token to refresh_access_token_db in services init

--- order_tracker_api/app/services/__init__.py
+++ order_tracker_api/app/services/__init__.py
@@ -1,10 +1,10 @@
-from app.services.auth_service import authenticate_user, create_tokens, refresh_access_token
+from app.services.auth_service import authenticate_user, create_tokens, refresh_access_token_db
 from app.services.order_service import get_orders, get_order_by_id
 
 __all__ = [
     "authenticate_user",
     "create_tokens",
-    "refresh_access_token",
+    "refresh_access_token_db",
     "get_orders",
     "get_order_by_id",
 ]
